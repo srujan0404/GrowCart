66fbb412df50166e9a15eeb02c37dedc5da17c08
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-09-24 11:55:21 +0530

    Added Login Button
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- basic.js
+++ basic.js
@@ -9,9 +9,31 @@ for (var i = 0; i < addtocarts.length; i++) {
   });
 }
 var buynow = document.getElementsByClassName("buy-btn");
-for (var i = 0; i < buynow.length; i++) {
+for (let i = 0; i < buynow.length; i++) {
   buynow.item(i).addEventListener("click", function (e) {
     //e.preventDefault(); /*use if you want to prevent the original link following action*/
-    alert("buynow");
+    //alert("hi");
+    let Imagesrc = 1;
+    if (i <= 15) {
+      Imagesrc = document.getElementsByClassName("img-fluid mb-3")[i].src;
+      window.location = "./sproduct.html?imgsrc=" + Imagesrc;
+    }
   });
 }
+
+//Login Page..
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//Fetch Data From Login page
+document.getElementById("login").addEventListener("click", function (e) {
+  let UserName = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  alert(pass);
+});
