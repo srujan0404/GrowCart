09b07dca9c5f3a7de657dab1fb957536a913b289
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-09-24 15:39:13 +0530

    Added Login Page and Attached Firebase
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- basic.js
+++ basic.js
@@ -1,3 +1,40 @@
+//For FireStore
+import { initializeApp } from "./firestore/firebase-app.js";
+//
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+//
+const firebaseConfig = {
+  apiKey: "AIzaSyBajPjgEiSEmGftWPb-BIEJbyAfVS2Y_FE",
+  authDomain: "payment-8d349.firebaseapp.com",
+  projectId: "payment-8d349",
+  storageBucket: "payment-8d349.appspot.com",
+  messagingSenderId: "718040561229",
+  appId: "1:718040561229:web:d64c90ce0da1fadfe70e04",
+};
+//
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  deleteField,
+} from "./firestore/firebase-firestore.js";
+//
+let UserName = "Default";
+let password = null;
+//
 function OpenShop() {
   window.location = "./shop.html";
 }
@@ -21,19 +58,33 @@ for (let i = 0; i < buynow.length; i++) {
   });
 }
 
-//Login Page..
-var modal = document.getElementById("id01");
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
 //Fetch Data From Login page
-document.getElementById("login").addEventListener("click", function (e) {
-  let UserName = document.getElementById("username").value;
-  let pass = document.getElementById("password").value;
-  alert(pass);
+// Initialize Firebase
+
+// Add event listener to the login button
+document.getElementById("login").addEventListener("click", function () {
+  AddDocument_AutoID();
 });
+
+// Add Documents
+async function AddDocument_AutoID() {
+  const UserName = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // alert(UserName + password);
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  // Replace 'state' with the actual value you want to set
+  const state = "some_state_value";
+
+  const ref = collection(db, "Data");
+
+  const docRef = await addDoc(ref, {
+    Username: UserName,
+    PassWord: password,
+    state: state,
+  });
+
+  //console.log("Document added with ID: ", docRef.id);
+}
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBajPjgEiSEmGftWPb-BIEJbyAfVS2Y_FE",
+  authDomain: "payment-8d349.firebaseapp.com",
+  projectId: "payment-8d349",
+  storageBucket: "payment-8d349.appspot.com",
+  messagingSenderId: "718040561229",
+  appId: "1:718040561229:web:d64c90ce0da1fadfe70e04",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
