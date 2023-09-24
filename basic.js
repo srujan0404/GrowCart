054720cf9e0e525547e751d464784ab52514bd1d
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2023-09-24 10:24:43 +0530

    Buttons Sucessfully Working
    Signed-off-by: Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>

--- basic.js
+++ basic.js
@@ -2,9 +2,16 @@ function OpenShop() {
   window.location = "./shop.html";
 }
 var addtocarts = document.getElementsByClassName("buy-btn-cart");
-for (var i = 0; i < hrefs.length; i++) {
-  hrefs.item(i).addEventListener("click", function (e) {
+for (var i = 0; i < addtocarts.length; i++) {
+  addtocarts.item(i).addEventListener("click", function (e) {
     //e.preventDefault(); /*use if you want to prevent the original link following action*/
     alert("hohoho");
+  });
+}
+var buynow = document.getElementsByClassName("buy-btn");
+for (var i = 0; i < buynow.length; i++) {
+  buynow.item(i).addEventListener("click", function (e) {
+    //e.preventDefault(); /*use if you want to prevent the original link following action*/
+    alert("buynow");
   });
 }
