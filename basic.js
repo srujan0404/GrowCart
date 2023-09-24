3e9a00a80013c057a0edd8c7979656017c4962aa
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-09-24 10:08:46 +0530

    Added js file Did Some Modifications
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

---
+++ basic.js
@@ -0,0 +1,10 @@
+function OpenShop() {
+  window.location = "./shop.html";
+}
+var addtocarts = document.getElementsByClassName("buy-btn-cart");
+for (var i = 0; i < hrefs.length; i++) {
+  hrefs.item(i).addEventListener("click", function (e) {
+    //e.preventDefault(); /*use if you want to prevent the original link following action*/
+    alert("hohoho");
+  });
+}
